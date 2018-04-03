a0b2e41a759a448e9248bdd3681db1333d53ca27
Sardo <sardo@pixelactionstudio.com>
2018-04-03 15:50:49 +0800

    Recover document page

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,12 @@
 <template>
   <div id="app">
     <Book>
+      <div class="page cover">
+        <article class="content">
+          <h1>Vue Booklet</h1>
+          <img class="img-responsive" src="./assets/black_cat_black_and_white-300px.png" />
+        </article>
+      </div>
       <div class="page">
         <article class="content">
           <h1>Hello World !</h1>
@@ -46,6 +52,33 @@
           Laoreet sit amet cursus sit amet dictum sit amet. Gravida dictum fusce ut placerat orci nulla.
         </article>
       </div>
+      <div class="page">
+        <article class="content">
+          <h1>It is page 4</h1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Augue ut lectus arcu bibendum at varius.
+          Ac odio tempor orci dapibus ultrices in iaculis. Euismod in pellentesque massa placerat.
+          Arcu felis bibendum ut tristique.
+          Orci a scelerisque purus semper eget duis at. Dictum fusce ut placerat orci nulla.
+          Rhoncus dolor purus non enim praesent. Auctor neque vitae tempus quam pellentesque nec.
+          Vitae turpis massa sed elementum tempus. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor.
+          Urna neque viverra justo nec ultrices dui sapien. Arcu cursus vitae congue mauris rhoncus.
+          Laoreet sit amet cursus sit amet dictum sit amet. Gravida dictum fusce ut placerat orci nulla.
+        </article>
+        <article class="content">
+          <h1>It is back of page 4</h1>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+          sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Augue ut lectus arcu bibendum at varius.
+          Ac odio tempor orci dapibus ultrices in iaculis. Euismod in pellentesque massa placerat.
+          Arcu felis bibendum ut tristique.
+          Orci a scelerisque purus semper eget duis at. Dictum fusce ut placerat orci nulla.
+          Rhoncus dolor purus non enim praesent. Auctor neque vitae tempus quam pellentesque nec.
+          Vitae turpis massa sed elementum tempus. Egestas tellus rutrum tellus pellentesque eu tincidunt tortor.
+          Urna neque viverra justo nec ultrices dui sapien. Arcu cursus vitae congue mauris rhoncus.
+          Laoreet sit amet cursus sit amet dictum sit amet. Gravida dictum fusce ut placerat orci nulla.
+        </article>
+      </div>
+      <div class="page back"></div>
     </Book>
   </div>
 </template>
